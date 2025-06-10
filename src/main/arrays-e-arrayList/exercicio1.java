4280348c6645f7c92fa68866c8ebb6d2e0e676ea
Joao Labussiere <labussework@gmail.com>
2025-06-10 10:05:19 -0300

    array exercício 1

---
+++ src/main/arrays-e-arrayList/exercicio1.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+//        Crie um programa que:
+//        Declare um array de 5 números inteiros
+//        Preencha com valores digitados pelo usuário
+//        Exiba todos os valores e a soma total
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> array = new ArrayList<Integer>();
+
+
+    }
+}
