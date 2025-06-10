d504b722afb99aaa24ddc50755e97319b37832de
JPLabussiereF <labussework@gmail.com>
2025-06-10 16:00:31 -0300

    Começo do exercício 2

--- src/main/arrays-e-arrayList/exercicio1.java
+++ src/main/arrays-e-arrayList/exercicio1.java
@@ -9,8 +9,27 @@ public class exercicio1 {
 //        Preencha com valores digitados pelo usuário
 //        Exiba todos os valores e a soma total
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> array = new ArrayList<Integer>();
 
+        int[] numeros = new int[5];
+
+        System.out.println("===LISTA DE 5 NÚMEROS INTEIROS===");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Digite o " + (i + 1) + "° número: ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        int soma = 0;
+        for (int numero : numeros) {
+            soma += numero;
+        }
+
+        System.out.println("===EXIBIÇÃO DOS "+ numeros.length+" NÚMEROS===");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("O "+(i + 1) + "° número é: " + numeros[i]);
+        }
+        System.out.println("===SOMA DOS "+ numeros.length+" NÚMEROS DIGITADOS===");
+        System.out.println("Soma: " + soma);
 
     }
 }
---
+++ src/main/arrays-e-arrayList/exercicio2.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class exercicio2 {
+    public static void main(String[] args) {
+//        Desenvolva um sistema que:
+//        Use ArrayList para armazenar nomes de alunos
+//        Permita adicionar, remover e listar alunos
+//        Implemente busca por nome (ignorando case)
+//        Exiba a quantidade total de alunos
+
+        ArrayList<String> alunos = new ArrayList<String>();
+
+    }
+}
